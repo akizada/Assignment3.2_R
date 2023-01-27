59fe0f5842054bdf692819f47862febca58f8d0a
akizada <123576168+akizada@users.noreply.github.com>
2023-01-27 10:24:15 +0100

    Add files via upload

---
+++ my_function_remind_me.R
@@ -0,0 +1,15 @@
+
+# FUNCTION: REMIND ME
+
+remind_me <- function() {
+  shopping_list <- c("rice", 
+                     "lentils", 
+                     "coconut milk",
+                     "carrots",
+                     "hummus",
+                     "oat milk",
+                     "ground coffee",
+                     "muesli")
+  print(shopping_list)
+}
+
