79628ca54bd89c7791ff8ee6676598164b77e41e
akizada <123576168+akizada@users.noreply.github.com>
2023-01-27 10:24:31 +0100

    Add files via upload

---
+++ my_function_cheat.R
@@ -0,0 +1,15 @@
+
+# FUNCTION: CHEAT
+
+cheat <- function(question) {
+  if (question == 1) {
+    browseURL("https://github.com/akizada/Assignment3.2_R/blob/main/Q3.1.1.R")
+  } else if (question == 2) {
+    browseURL("https://github.com/akizada/Assignment3.2_R/blob/main/Q3.1.2.R")
+  } else if (question == 3) {
+    browseURL("https://github.com/akizada/Assignment3.2_R/blob/main/Q3.1.3.R")
+  }
+}
+
+
+
